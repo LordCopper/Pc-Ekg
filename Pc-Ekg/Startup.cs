8fc78350f906cb10c462fddc3772b6b437661a1f
threezool <threezool@gmail.com>
2015-09-24 09:53:18 +0200

    Initial commit

---
+++ Pc-Ekg/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Pc_Ekg.Startup))]
+namespace Pc_Ekg
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
